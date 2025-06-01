4de09feedf8b392f8a3b37df1cb8449747d08508
DaNike <danike@cirr.com>
2025-06-01 04:11:49 -0500

    Add manually-triggered release pipeline

---
+++ tools/bump_version.ps1
@@ -0,0 +1,61 @@
+#Requires -Version 7
+
+param (
+    [ValidateSet('Major', 'Minor', 'Patch')]
+    [string] $BumpVersion = 'Patch'
+)
+
+$ErrorActionPreference = 'Stop';
+Set-StrictMode -Version 3;
+
+$VersionIndex = 0;
+if ($BumpVersion -eq 'Major')
+{
+    $VersionIndex = 0;
+}
+elseif ($BumpVersion -eq 'Minor')
+{
+    $VersionIndex = 1;
+}
+elseif ($BumpVersion -eq 'Patch')
+{
+    $VersionIndex = 2;
+}
+
+Push-Location $PSScriptRoot;
+Set-Location ../;
+try
+{
+    # load the project up as a normal XML file
+    $xml = [xml]::new();
+    $xml.PreserveWhitespace = $true;
+    $xml.Load("Version.props");
+
+    $version = $xml.Project.PropertyGroup.VersionPrefix;
+    if ($VersionIndex -eq 0)
+    {
+        $xml.Project.PropertyGroup.PackageValidationBaselineVersion = "";
+    }
+    else
+    {
+        $xml.Project.PropertyGroup.PackageValidationBaselineVersion = $version;
+    }
+
+    $verParts = $version -csplit '\.',3;
+    $verParts[$VersionIndex] = [string](([int]$verParts[$VersionIndex]) + 1);
+    for ($i = $VersionIndex + 1; $i -lt $verParts.Length; $i++)
+    {
+        $verParts[$i] = '0';
+    }
+    $version = $verParts -join '.';
+    $xml.Project.PropertyGroup.VersionPrefix = $version
+
+    Write-Host "New version: $version";
+
+    # and write the project file back out
+    $xml.Save("Version.props");
+}
+finally
+{
+    Pop-Location;
+}

51ab917936b80bb429760712aadff53928d9c7cc
DaNike <danike@cirr.com>
2025-06-03 19:53:43 -0500

    Pull nullable attributes and IsExternalInit into Backports publicly

---
+++ src/MonoMod.Backports/System/Diagnostics/CodeAnalysis/NullableAttributes,gte_core_3.0,gte_std_2.1.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(AllowNullAttribute))]
+[assembly: TypeForwardedTo(typeof(DisallowNullAttribute))]
+[assembly: TypeForwardedTo(typeof(MaybeNullAttribute))]
+[assembly: TypeForwardedTo(typeof(NotNullAttribute))]
+[assembly: TypeForwardedTo(typeof(MaybeNullWhenAttribute))]
+[assembly: TypeForwardedTo(typeof(NotNullWhenAttribute))]
+[assembly: TypeForwardedTo(typeof(NotNullIfNotNullAttribute))]
+[assembly: TypeForwardedTo(typeof(DoesNotReturnAttribute))]
+[assembly: TypeForwardedTo(typeof(DoesNotReturnIfAttribute))]
---
+++ src/MonoMod.Backports/System/Diagnostics/CodeAnalysis/NullableMemberAttributes,gte_core_5.0.cs
@@ -0,0 +1,5 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(MemberNotNullAttribute))]
+[assembly: TypeForwardedTo(typeof(MemberNotNullWhenAttribute))]
